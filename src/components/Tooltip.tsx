93d4fc3b24e321b15e758b98c01720cb0d315735
mayone-du <cocomayo1201@icloud.com>
2021-10-11 19:04:15 +0900

    Fix Tooltip component

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -4,29 +4,33 @@ type Props = {
   tooltipText: string;
 };
 
+// ツールチップ
 export const Tooltip: React.FC<Props> = memo((props) => {
   const ref = useRef<HTMLDivElement>(null);
   const handleMouseEnter = () => {
     if (!ref.current) return;
     ref.current.style.opacity = "1";
+    ref.current.style.visibility = "visible";
   };
   const handleMouseLeave = () => {
     if (!ref.current) return;
     ref.current.style.opacity = "0";
+    ref.current.style.visibility = "hidden";
   };
+
   return (
-    <div
-      className="flex relative items-center"
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-    >
+    <div className="flex relative items-center">
       <div
-        className="flex before:block absolute before:absolute top-full before:-top-1 left-1/2 before:left-1/2 z-10 before:z-0 items-center py-[2px] px-2 mx-auto mt-2 before:w-2 before:h-2 text-xs text-white whitespace-nowrap before:bg-black bg-black rounded transition-all duration-150 transform before:transform before:rotate-45 -translate-x-1/2 before:-translate-x-1/2"
+        className="flex before:block absolute before:absolute top-full before:-top-1 left-1/2 before:left-1/2 invisible z-10 before:z-0 items-center py-[2px] px-2 mx-auto mt-2 before:w-2 before:h-2 text-xs text-white whitespace-nowrap before:bg-black bg-black rounded transition-all duration-150 transform before:transform before:rotate-45 -translate-x-1/2 before:-translate-x-1/2"
         ref={ref}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
       >
         {props.tooltipText}
       </div>
-      {props.children}
+      <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+        {props.children}
+      </div>
     </div>
   );
 });
